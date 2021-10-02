cfa41000288a8e4bbb432120f251b010655f2fe4
wc <2019012617@bistu.edu.cn>
2021-10-02 11:35:40 +0800

    基本完成扫码登录功能，界面细节待优化

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,90 +1,15 @@
 <template>
   <div class="login-container">
     <div id="weixin"></div>
-
     <span id="errMsg">{{errMsg}}</span>
-
-    <!--<el-form ref="loginForm" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on"-->
-    <!--label-position="left">-->
-
-    <!--<div class="title-container">-->
-    <!--<h3 class="title">Login Form</h3>-->
-    <!--</div>-->
-
-    <!--<el-form-item prop="username">-->
-    <!--<span class="svg-container">-->
-    <!--<svg-icon icon-class="user"/>-->
-    <!--</span>-->
-    <!--<el-input-->
-    <!--ref="username"-->
-    <!--v-model="loginForm.username"-->
-    <!--placeholder="Username"-->
-    <!--name="username"-->
-    <!--type="text"-->
-    <!--tabindex="1"-->
-    <!--auto-complete="on"-->
-    <!--/>-->
-    <!--</el-form-item>-->
-
-    <!--<el-form-item prop="password">-->
-    <!--<span class="svg-container">-->
-    <!--<svg-icon icon-class="password"/>-->
-    <!--</span>-->
-    <!--<el-input-->
-    <!--:key="passwordType"-->
-    <!--ref="password"-->
-    <!--v-model="loginForm.password"-->
-    <!--:type="passwordType"-->
-    <!--placeholder="Password"-->
-    <!--name="password"-->
-    <!--tabindex="2"-->
-    <!--auto-complete="on"-->
-    <!--@keyup.enter.native="handleLogin"-->
-    <!--/>-->
-    <!--<span class="show-pwd" @click="showPwd">-->
-    <!--<svg-icon :icon-class="passwordType === 'password' ? 'eye' : 'eye-open'"/>-->
-    <!--</span>-->
-    <!--</el-form-item>-->
-
-    <!--<el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;"-->
-    <!--@click.native.prevent="handleLogin">Login-->
-    <!--</el-button>-->
-
-    <!--<div class="tips">-->
-    <!--<span style="margin-right:20px;">username: admin</span>-->
-    <!--<span> password: any</span>-->
-    <!--</div>-->
-
-    <!--</el-form>-->
   </div>
 </template>
 <script>
-  import {validUsername} from '@/utils/validate'
-  import RemoteScript from '@/components/Common/remote-script'
   import request from "@/utils/request";
-
 
   export default {
     name: 'Login',
-    components: {
-      RemoteScript
-    },
-    mounted() {
-
-      // //权宜之计，便于手动获取code,方便调试接口
-      // !function (a, b, c) {
-      //   function d(a) {
-      //     var c = "default";
-      //     a.self_redirect === !0 ? c = "true" : a.self_redirect === !1 && (c = "false");
-      //     var d = b.createElement("iframe"),
-      //       e = "https://open.weixin.qq.com/connect/qrconnect?appid=" + a.appid + "&scope=" + a.scope + "&redirect_uri=" + a.redirect_uri + "&state=" + a.state + "&login_type=jssdk&self_redirect=" + c + '&styletype=' + (a.styletype || '') + '&sizetype=' + (a.sizetype || '') + '&bgcolor=' + (a.bgcolor || '') + '&rst=' + (a.rst || '');
-      //     e += a.style ? "&style=" + a.style : "", e += a.href ? "&href=" + a.href : "", d.src = e, d.frameBorder = "0", d.allowTransparency = "true", d.scrolling = "no", d.width = "300px", d.height = "400px";
-      //     var f = b.getElementById(a.id);
-      //     f.innerHTML = "", f.appendChild(d)
-      //   }
-      //
-      //   a.WxLogin = d
-      // }(window, document);
+    created() {
 
       request({
         url: "/utils/wxLoginJsFile",
@@ -108,33 +33,8 @@
     },
 
     data() {
-      const validateUsername = (rule, value, callback) => {
-        if (!validUsername(value)) {
-          callback(new Error('Please enter the correct user name'))
-        } else {
-          callback()
-        }
-      }
-      const validatePassword = (rule, value, callback) => {
-        if (value.length < 6) {
-          callback(new Error('The password can not be less than 6 digits'))
-        } else {
-          callback()
-        }
-      }
       return {
-
         errMsg:undefined,
-        loginForm: {
-          username: 'admin',
-          password: '111111'
-        },
-        loginRules: {
-          username: [{required: true, trigger: 'blur', validator: validateUsername}],
-          password: [{required: true, trigger: 'blur', validator: validatePassword}]
-        },
-        loading: false,
-        passwordType: 'password',
         redirect: undefined
       }
     },
@@ -152,37 +52,6 @@
       }
     },
     methods: {
-      showPwd() {
-        if (this.passwordType === 'password') {
-          this.passwordType = ''
-        } else {
-          this.passwordType = 'password'
-        }
-        this.$nextTick(() => {
-          this.$refs.password.focus()
-        })
-      },
-      handleLogin() {
-        this.$refs.loginForm.validate(valid => {
-          if (valid) {
-            this.loading = true
-
-            this.$store.dispatch('user/login', this.loginForm)
-              .then(() => {
-                console.log("login success")
-                console.log(this.redirect)
-                this.$router.push({path: this.redirect || '/'})
-                console.log("route success")
-                this.loading = false
-              }).catch(() => {
-              // this.loading = false
-            })
-          } else {
-            console.log('error submit!!')
-            return false
-          }
-        })
-      }
     }
   }
 </script>
